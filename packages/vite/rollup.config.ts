cf440d2413b51f8a9f454edd6c5f79fb4f2d6188
Daniel Roe <daniel@roe.dev>
2022-06-17 15:12:03 +0100

    fix: avoid circular polyfill

--- packages/vite/rollup.config.ts
+++ packages/vite/rollup.config.ts
@@ -405,10 +405,10 @@ import { fileURLToPath as __cjs_fileURLToPath } from 'url';
 import { dirname as __cjs_dirname } from 'path';
 import { createRequire as __cjs_createRequire } from 'module';
 
+const __require = __cjs_createRequire(import.meta.url);
+const require = __require;
 const __filename = __cjs_fileURLToPath(import.meta.url);
 const __dirname = __cjs_dirname(__filename);
-const require = __cjs_createRequire(import.meta.url);
-const __require = require;
 `.trimStart()
 
   return {
